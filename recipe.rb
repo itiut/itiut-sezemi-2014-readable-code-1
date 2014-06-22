ca47dddfaa33d43a96ae7f0a9b1eefc36658c56d
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-06-22 11:43:17 +0900

    spec5: ARGV[0]を説明変数data_fileに代入

--- recipe.rb
+++ recipe.rb
@@ -4,12 +4,14 @@ def usage
   puts "Usage: #{$PROGRAM_NAME} recipe-data.txt [id]"
 end
 
-unless ARGV[0]
+data_file = ARGV[0]
+unless data_file
   usage
   exit
 end
 
 File.open(ARGV[0]) do |file|
+File.open(data_file) do |file|
   file.each_with_index do |line, i|
     puts "#{i + 1}: #{line}"
   end
