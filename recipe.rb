86c319d9addfe20f8eb7f4a096c78a947922caf2
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-06-22 11:50:11 +0900

    spec7: sort lines

--- recipe.rb
+++ recipe.rb
@@ -4,16 +4,16 @@ def usage
   puts "Usage: #{$PROGRAM_NAME} recipe-data.txt [id]"
 end
 
+def id_is_specified?
+  !!ARGV[1]
+end
+
 data_file = ARGV[0]
 specified_id = ARGV[1].to_i
 
 unless data_file
   usage
   exit
-end
-
-def id_is_specified?
-  !!ARGV[1]
 end
 
 File.open(data_file) do |file|
